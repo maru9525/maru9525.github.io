1fde6267f58284e9794952cd19fbb4d46b557a48
maru9525 <cember123@gmail.com>
2025-01-03 01:05:45 +0900

    오류 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735832400',resources: [
+    cacheName: 'chirpy-1735833194',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
